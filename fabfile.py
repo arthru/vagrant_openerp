158bd4575e0c9334ae18959f6d06a48aee605648
Arthur Vuillard <arthur.vuillard@gmail.com>
2013-03-26 22:52:02 +0100

    Fabfile and config files (and template) for rsyslog conf

--- fabfile.py
+++ fabfile.py
@@ -51,3 +51,48 @@ def openerp():
     #require.deb.package('cpenerp')
     if not deb.is_installed('openerp'):
         deb.install('openerp', options=['--force-yes'])
+
+@task
+def openerp_rsyslog():
+    require.files.file(
+        '/etc/openerp/openerp-server.conf', 
+        source='files/etc/openerp/openerp-server.conf', 
+        owner = 'openerp', 
+        group='openerp', 
+        mode=640, 
+        use_sudo= True
+    )
+    require.files.file(
+        '/etc/rsyslog.d/20-openerp.conf', 
+        source='files/etc/rsyslog.d/20-openerp.conf', 
+        owner='root', 
+        group='root', 
+        mode=644, 
+        use_sudo = True
+    )
+    require.files.file(
+        '/etc/init.d/openerp',
+        source='files/etc/init.d/openerp',
+        owner='root',
+        group='root',
+        mode='755',
+        use_sudo=True
+    )
+    require.files.template_file(
+        '/usr/local/bin/mail-openerp-err.py',
+        template_source='files/usr/local/bin/mail-openerp-err.py',
+        context = {
+            'smtp_ssl':None,
+            'smtp_host':None,
+            'smtp_port':None,
+            'smtp_user':None,
+            'smtp_password':None,
+            'mail_to':None
+        }, 
+        owner='root',
+        group='root', 
+        mode = 755, 
+        use_sudo=True 
+    )
+    require.service.restarted('rsyslog')
+    require.service.restarted('openerp')
--- files/usr/local/bin/mail-openerp-err.py
+++ files/usr/local/bin/mail-openerp-err.py
@@ -5,14 +5,14 @@ A simple tool that gets a message as first argument and send it by mail
 '''
 # Configuration
 
-SMTP_SSL=False
-SMTP_HOST=''
-SMTP_PORT=25
-SMTP_USER=None
-SMTP_PASSWORD=None
+SMTP_SSL=%(smtp_ssl)s
+SMTP_HOST='%(smtp_host)s'
+SMTP_PORT=$(smtp_port)d
+SMTP_USER='$(smtp_user)s'
+SMTP_PASSWORD='$(smtp_password)s'
 
 MAIL_FROM='OpenERP Server'
-MAIL_TO= ['']
+MAIL_TO= ['$(mail_to)s']
 
 # Code
 
@@ -39,11 +39,11 @@ if SMTP_USER and SMTP_PASSWORD:
     conn.login(SMTP_USER, SMTP_PASSWORD)
 
 MAIL_CONTENT='''Subject: [OPENERP] Server Error
-From: %s
-To: %s
+From: %%s
+To: %%s
 
-%s 
-''' % (MAIL_FROM, ", ".join(MAIL_TO), LOG_MESSAGE)
+%%s 
+''' %% (MAIL_FROM, ", ".join(MAIL_TO), LOG_MESSAGE)
 
 conn.sendmail(MAIL_FROM,MAIL_TO,MAIL_CONTENT)
 conn.quit()
